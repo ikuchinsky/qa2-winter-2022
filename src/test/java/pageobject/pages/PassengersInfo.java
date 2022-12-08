d93db2e1ea32e785851aa89ad8971aa79740c15a
Ilya Kucinskis <ikuchinsky@inbox.lv>
2022-12-08 19:38:56 +0200

    HOMEWORK TICKETS

---
+++ src/test/java/pageobject/pages/PassengersInfo.java
@@ -0,0 +1,11 @@
+package pageobject.pages;
+
+import pageobject.BaseFunc;
+
+public class PassengersInfo {
+    private BaseFunc baseFunc;
+
+    public PassengersInfo(BaseFunc baseFunc) {
+        this.baseFunc = baseFunc;
+    }
+}
